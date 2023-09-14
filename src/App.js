4607545de829b223371004357b4503971efb4a46
IG <iikim2511@gmail.com>
2023-09-13 23:59:08 -0400

    redux--ing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import './App.css';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore(()=>{return [{id:0, name:test}]})
 
 const App = () => {
   return (
-    <div>App</div>
+
+    <Provider store={store}>
+        <div>App
+        
+        sdfgdsf
+        
+        
+        </div>
+    </Provider>
   )
 }
 
